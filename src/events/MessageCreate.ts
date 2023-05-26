1a000ab469210afb3db0353102770efef25ee552
txj-xyz <7461476+txj-xyz@users.noreply.github.com>
2023-05-26 16:01:22 -0600

    fix: include the slash command builder

---
+++ src/events/MessageCreate.ts
@@ -0,0 +1,30 @@
+import { Message } from "discord.js";
+import { BotEvent } from "../abstract/BotEvent";
+
+export default class MessageCreate extends BotEvent {
+  get name(): string {
+    return "messageCreate";
+  }
+
+  get fireOnce(): boolean {
+    return false;
+  }
+
+  get enabled(): boolean {
+    return true;
+  }
+
+  async run([message]: [Message]): Promise<void | Message<true>> {
+    if (message.author.bot) return;
+    if (!message.inGuild()) return;
+    const [cmd] = message.content.split(" ");
+
+    if (
+      message.mentions.users.first()?.id == this.client.user?.id &&
+      this.client.util.config.owners.includes(message.author.id) &&
+      message.content.includes(`build`)
+    ) {
+      return await this.client.util.slashCommandBuilder(message);
+    }
+  }
+}
